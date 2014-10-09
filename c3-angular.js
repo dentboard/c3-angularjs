8f892a05c7d21ef83258dccbec898677c204d583
Sanket Chauhan <sanke93@gmail.com>
2014-10-09 18:05:12 -0400

    finished generic directivefor c3

--- c3-angular.js
+++ c3-angular.js
@@ -1,29 +1,70 @@
-angular.module('c3-angularjs', [])
-	.directive('c3Chart', function() {
-        return {
-            restrict: 'AE',
-            scope:{
-            	data: '@',
-            	type: '@', 
-            	chartid: '@'
-            },
-            template: "<div></div>",
-            link: function(scope, elem, attrs) {
+/**
+ * @name C3 angularjs directive
+ * @desc Generic directive along with directives for commonly used charts
+ */
+var counter = Math.ceil((Math.random() * 1000));
+(function(){
+	angular.module('c3-angularjs', [])
+		.directive('c3Chart', function() {
+	        return {
+	            restrict: "AE",
+	            scope:{
+	            	data: "@",
+	            	axis: "@", 
+	            	chartid: "@",
+	            	color: "@", 
+	            	size: "@",
+	            	padding: "@",
+	            	grid: "@",
+	            },
+	            template: "<div></div>",
+	            link: function(scope, elem, attrs) {
 
-            	elem_dir= elem;
-            	attr_dir=attrs;
-            	scope_dir = scope;
+	            	elem_dir= elem;
+	            	attr_dir=attrs;
+	            	scope_dir = scope;
 
-            	//set the id
-            	elem[0].childNodes[0].id = scope.chartid;
-            	console.log('scope data', '#'+scope.chartid);
-            	
-            	var chart = c3.generate({
-            	    bindto: '#'+scope.chartid,
-            	    data: JSON.parse(scope.data)
-            	});
-            	
-            },
-    };
-});
+	            	//set the id if given or add a random one
+	            	if (scope.chartid){
+	            		elem[0].childNodes[0].id = scope.chartid;
+	            	}
+	            	else{
+	            		elem[0].childNodes[0].id = scope.chartid = 'c3'+counter;
+	            		counter++;
+	            	}
 
+	            	axis = scope.axis || "{}"
+	            	color = scope.color || "{}"
+	            	size = scope.size || "{}"
+	            	padding = scope.padding || "{}"
+	            	grid = scope.grid || "{}"
+
+	            	//generate the chart
+	            	var chart = c3.generate({
+	            	    bindto: '#'+scope.chartid,
+	            	    data: JSON.parse(scope.data),
+	            	    axis: JSON.parse(axis),
+	            	    color: JSON.parse(color),
+	            	    size: JSON.parse(size),   
+	            	    padding: JSON.parse(padding),
+	            	    grid: JSON.parse(grid)         	    
+	            	});
+
+	            	chart1 = chart;
+	            	//console.log('chart', chart);
+
+	            	//update 
+	            	scope.$watch('data', function(newVal, oldVal) {
+	            		if(newVal != oldVal){
+		            		//console.log('in data watch', typeof(newVal), oldVal);
+		            		chart.unload();
+		            		chart.load(
+		            			JSON.parse(newVal)
+		            		);
+		            	}
+	            	});
+
+	            },
+	    };
+	});
+}());
--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-var app = angular.module("test", ['c3-angularjs']);
-
-app.controller("Ctrl",function($scope){
+function MainCtrl ($scope) {
     scope_ctrl = $scope;
-    $scope.data = {
+    $scope.data =  {
       columns: [
         ['data1', 30, 200, 100, 400, 150, 250],
         ['data2', 50, 20, 10, 40, 15, 25]
-      ]
+      ],
+      axes: {
+        data2: 'y2'
+      },
+      types: {
+        data2: 'bar'
+      }
+    };
+    $scope.axis = {
+      y: {
+        label: {
+          text: 'Y Label',
+          position: 'outer-middle'
+        },
+        tick: {
+          format: d3.format("$,") // ADD
+        }
+      },
+      y2: {
+        show: true,
+        label: {
+          text: 'Y2 Label',
+          position: 'outer-middle'
+        }
+      }
+    }
+
+    $scope.options = {
+        axes:{}
     };
 
-    $scope.type = 'bar';
-});
+    $scope.size =  { width: 500 }
+}
+
+
+angular.module("test", ['c3-angularjs'])
+    .controller('Ctrl', MainCtrl);
